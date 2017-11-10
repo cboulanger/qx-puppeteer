ae4535187a1bae232f969ee8808e653fed9ac38c
Christian M. Boulanger <info@bibliograph.org>
2017-11-10 12:56:47 +0100

    Updated console messages

--- tests/demo.spec.js
+++ tests/demo.spec.js
@@ -4,8 +4,11 @@ import puppeteer from "puppeteer";
 import httpServer from "http-server";
 import * as path from "path";
 
+const build_target = "source";
+const host = "127.0.0.1";
+const port = 8080;
 const root = path.join(__dirname, '..', 'demo');
-const url  = "http://127.0.0.1:8080/source-output/demo/";
+const url  = `http://${host}:${port}/${build_target}-output/demo/`;
 
 let page;
 let browser;
@@ -14,17 +17,25 @@ const width = 600;
 const height = 400;
 
 beforeAll(async () => {
-  jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 50000;
   server = httpServer.createServer({ root });
-  server.listen(8080);
-  browser = await puppeteer.launch({
-    headless: false,
-    slowMo: 80,
-    args: [`--window-size=${width},${height}`]
-  });
-  page = await browser.newPage();
-  await page.setViewport({ width, height });
-  await page.goto(url);
+  server.listen(port);
+  console.info(`Started a server on ${host}:${port}`);
+  try {
+    
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 80,
+      args: [`--window-size=${width},${height}`]
+    });
+    page = await browser.newPage();
+    await page.setViewport({ width, height });
+    console.info(`Navigating to ${url}...`);
+    await page.goto(url);
+    console.info(`Opened ${url}...`);  
+  } catch(e) {
+    console.error(e);
+  }
 });
 
 describe("Testing the frontend",  () => {
